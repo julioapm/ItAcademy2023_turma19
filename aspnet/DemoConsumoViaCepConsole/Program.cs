63c1e04af85b37d34c7389e0d047f75a7c22ed46
Júlio Machado <julioapm@gmail.com>
2023-08-15 15:16:26 -0300

    DemoConsumoViaCepConsole V3

--- aspnet/DemoConsumoViaCepConsole/Program.cs
+++ aspnet/DemoConsumoViaCepConsole/Program.cs
@@ -6,6 +6,17 @@
     Console.WriteLine(resultado.StatusCode);
     var dados = await resultado.Content.ReadAsStringAsync();
     Console.WriteLine(dados);
+
+    Console.WriteLine("ConsultarV2:");
+    var resultadoV2 = await ViaCepConsumidor.ConsultarV2("01001000");
+    Console.WriteLine(resultadoV2);
+
+    Console.WriteLine("ConsultarV3:");
+    var resultadoV3 = await ViaCepConsumidor.ConsultarV3("01001000");
+    if (resultadoV3 != null) {
+        Console.WriteLine(resultadoV3.Cep);
+        Console.WriteLine(resultadoV3.Uf);
+    }
 }
 catch (Exception e)
 {
--- aspnet/DemoConsumoViaCepConsole/ViaCep.cs
+++ aspnet/DemoConsumoViaCepConsole/ViaCep.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Json;
+
 public class ViaCepConsumidor
 {
     public const string URI_BASE = "https://viacep.com.br/ws";
@@ -8,5 +10,26 @@ public class ViaCepConsumidor
         string uri = $"{URI_BASE}/{cep}/json";
         return cliente.GetAsync(uri);
     }
-    
+
+    public static Task<string> ConsultarV2(string cep)
+    {
+        string uri = $"{URI_BASE}/{cep}/json";
+        return cliente.GetStringAsync(uri);
+    }
+
+    public static Task<ViaCepDTO?> ConsultarV3(string cep)
+    {
+        string uri = $"{URI_BASE}/{cep}/json";
+        return cliente.GetFromJsonAsync<ViaCepDTO>(uri);
+    }
+}
+
+public class ViaCepDTO
+{
+    public string? Cep {get;set;}
+    public string? Logradouro {get;set;}
+    public string? Complemento {get;set;}
+    public string? Bairro {get;set;}
+    public string? Localidade {get;set;}
+    public string? Uf {get;set;}
 }
