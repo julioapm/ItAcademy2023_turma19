49482bd76af1bf1323758abe4fb07615ee87b101
Júlio Machado <julioapm@gmail.com>
2023-08-16 17:03:54 -0300

    DemoTemplateMVC

---
+++ aspnet/DemoTemplateMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DemoTemplateMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
