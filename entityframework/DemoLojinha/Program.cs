98e0ec52275a9b3b7c4da80d7d1c830cd7b98290
Júlio Machado <julioapm@gmail.com>
2023-08-28 15:34:17 -0300

    DemoLojinha migração

--- entityframework/DemoLojinha/Program.cs
+++ entityframework/DemoLojinha/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using DemoLojinha.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<LojinhaContext>(opcoes => {
+    opcoes.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opcoes.EnableSensitiveDataLogging().LogTo(Console.WriteLine);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
