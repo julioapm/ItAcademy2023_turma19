b4a0e29a81f756d893ad25604d2aa11bd96f325d
Júlio Machado <julioapm@gmail.com>
2023-08-28 15:03:56 -0300

    DemoLojinha models

---
+++ entityframework/DemoLojinha/Models/Produto.cs
@@ -0,0 +1,12 @@
+namespace DemoLojinha.Models;
+
+public class Produto
+{
+    public int Id {get;set;}
+    public string Nome {get;set;} = null!;
+    public string? Descricao {get;set;}
+    public int PrecoUnitario {get;set;}
+    //relacionamento N-N com Pedido
+    public ICollection<Pedido> Pedidos {get;set;} = null!;
+    public List<Item> Itens {get;set;} = null!;
+}
