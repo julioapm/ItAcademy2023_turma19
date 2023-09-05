7f1c835ecf71c257c4b0b8138759aad49942b7b8
Júlio Machado <julioapm@gmail.com>
2023-09-05 16:23:04 -0300

    demo_typescript_colecoes atualizacao

--- typescript/demo_typescript_colecoes/src/index.ts
+++ typescript/demo_typescript_colecoes/src/index.ts
@@ -30,3 +30,30 @@ const resultado3 = palavras.map(p => p.length);
 console.log(resultado3);
 const resultado4 = numeros.reduce((valorAnterior,valorAtual) => valorAnterior+valorAtual,0);
 console.log(resultado4);
+
+const mapa = new Map<string,string>();
+mapa.set('RS', 'Rio Grande Sul');
+mapa.set('SC', 'Santa Catarina');
+mapa.set('PR', 'Paraná');
+console.log(mapa.get('RS'));
+for(let e of mapa) {
+    console.log(e);
+}
+for(let sigla of mapa.keys()) {
+    console.log(sigla);
+}
+for(let estado of mapa.values()) {
+    console.log(estado);
+}
+
+const conjunto = new Set<number>();
+conjunto.add(1);
+conjunto.add(2);
+conjunto.add(1);
+console.log(conjunto.size);
+
+const conjuntomisto = new Set<number|string>();
+conjuntomisto.add(1);
+conjuntomisto.add('teste');
+conjuntomisto.forEach(e => console.log(e));
+console.log(conjuntomisto.has(3));
