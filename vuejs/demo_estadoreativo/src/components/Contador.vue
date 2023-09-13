b4b86995aefbd62aee52e671a726107d940d9aca
Júlio Machado <julioapm@gmail.com>
2023-09-13 17:05:34 -0300

    demo_estadoreativo final

---
+++ vuejs/demo_estadoreativo/src/components/Contador.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const contagem = ref(0);
+
+function incrementarContagem() {
+    contagem.value++;
+}
+
+function tratadorDoClick() {
+    incrementarContagem();
+}
+</script>
+
+<template>
+    <div>
+        <button type="button" @click="tratadorDoClick">Clique Aqui</button>
+        <span>foi clicado {{ contagem }} vezes</span>
+    </div>
+</template>
