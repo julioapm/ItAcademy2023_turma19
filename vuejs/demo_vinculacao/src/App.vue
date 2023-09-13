9eaebb66dd7be41fc921c13cd5c9eb235c16b205
Júlio Machado <julioapm@gmail.com>
2023-09-13 16:38:19 -0300

    demo_vinculacao final

--- vuejs/demo_vinculacao/src/App.vue
+++ vuejs/demo_vinculacao/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AloMundo from './components/AloMundo.vue';
 import AloMundo2 from './components/AloMundo2.vue';
+import AloMundo3 from './components/AloMundo3.vue';
 </script>
 
 <template>
@@ -9,6 +10,8 @@ import AloMundo2 from './components/AloMundo2.vue';
   <AloMundo />
   <br />
   <AloMundo2 />
+  <br />
+  <AloMundo3 />
 </template>
 
 <style scoped></style>
---
+++ vuejs/demo_vinculacao/src/components/AloMundo3.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    function tratadorDoClick(evento: Event) {
+        alert('Alô, Mundo!');
+    }
+    function tratadorDoClick2(evento: Event) {
+        const botaoClicado = evento.currentTarget as HTMLButtonElement;
+        alert(`Oi, ${botaoClicado.name}!`);
+    }
+</script>
+
+<template>
+    <button type="button" @click="tratadorDoClick">Diga Alô</button>
+    <button type="button" name="botaoOi" @click="tratadorDoClick2">Diga Oi</button>
+</template>
