d36737897fb55f329ae1a07e6c0b60251747607f
Júlio Machado <julioapm@gmail.com>
2023-09-15 16:03:52 -0300

    demo_props inicial

---
+++ vuejs/demo_slots/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import BotaoColorido from './components/BotaoColorido.vue';
+</script>
+
+<template>
+    <button type="button">Clique</button>
+    <BotaoColorido>Clique Aqui</BotaoColorido>
+    <BotaoColorido>
+        <span style="color: black;">Curtir</span>💕
+    </BotaoColorido>
+</template>
+
+<style scoped>
+</style>
---
+++ vuejs/demo_slots/src/components/BotaoColorido.vue
@@ -0,0 +1,17 @@
+<template>
+    <button type="button" class="botao-colorido">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.botao-colorido {
+    color: white;
+    background: linear-gradient(315deg, #42d392 25%, #647eff);
+    border: none;
+    padding: 5px 10px;
+    margin: 5px;
+    border-radius: 8px;
+    cursor: pointer;
+}
+</style>
