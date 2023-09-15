c293f5f35f4be016dee20cf0321e24b948fe3f22
Júlio Machado <julioapm@gmail.com>
2023-09-15 16:37:13 -0300

    demo_slots multiplos slots

--- vuejs/demo_slots/src/App.vue
+++ vuejs/demo_slots/src/App.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import BotaoColorido from './components/BotaoColorido.vue';
+import LayoutBasico from './components/LayoutBasico.vue';
 </script>
 
 <template>
-    <button type="button">Clique</button>
-    <BotaoColorido>Clique Aqui</BotaoColorido>
-    <BotaoColorido>
-        <span style="color: black;">Curtir</span>💕
-    </BotaoColorido>
+    <LayoutBasico>
+        <template #cabecalho>
+            <h1>Exemplos de Slots</h1>
+        </template>
+        <template #default>
+            <button type="button">Clique</button>
+            <BotaoColorido>Clique Aqui</BotaoColorido>
+            <BotaoColorido>
+                <span style="color: black;">Curtir</span>💕
+            </BotaoColorido>
+        </template>
+        <template #rodape>
+            <p>Desenvolvido por Eu Mesmo</p>
+        </template>
+    </LayoutBasico>
 </template>
 
 <style scoped>
---
+++ vuejs/demo_slots/src/components/LayoutBasico.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <slot name="cabecalho"></slot>
+    </header>
+    <main>
+        <slot></slot>
+    </main>
+    <footer>
+        <slot name="rodape"></slot>
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        border-top: 3px solid #ccc;
+        color: grey;
+        font-size: 0.8em;
+    }
+</style>
